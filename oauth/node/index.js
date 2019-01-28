eff3efcb7c6e9d37185bd963f7e116d98a764f37
Edward Paget <edward@cassetteta.pe>
2019-01-28 15:17:39 -0500

    Update sample oauth code and add readme

--- oauth/node/index.js
+++ oauth/node/index.js
@@ -1,30 +1,32 @@
 const express = require('express');
+const session = require('express-session');
 const { Issuer, Strategy } = require('openid-client');
 const passport = require('passport');
-const process = require('process');
-const session = require('express-session');
+const https = require('https');
 
-const ROOT_URL = 'https://deptva-eval.okta.com/';
-const secret = "oauth_test";
+const ROOT_URL = 'https://dev-api.va.gov/oauth2/.well-known/openid-configuration';
+const client_id = 'FAKE CLIENT ID';
+const client_secret = 'FAKE CLIENT SECRET';
 
-function createClient() {
+const createClient = async () => {
+  Issuer.defaultHttpOptions = { timeout: 2500 };
   return Issuer.discover(ROOT_URL).then(issuer => {
     return new issuer.Client({
-      client_id: process.env.VETS_API_CLIENT_ID,
-      client_secret: process.env.VETS_API_CLIENT_SECRET,
+      client_id,
+      client_secret,
       redirect_uris: [
-        'http://localhost:8080/auth/cb',
+        'http://localhost:8080/auth/cb'
       ],
     });
   });
 }
 
-function configurePassport(client) {
-  passport.serializeUser(function(user, done) {
+const configurePassport = (client) => {
+  passport.serializeUser((user, done) => {
     done(null, user);
   });
 
-  passport.deserializeUser(function(user, done) {
+  passport.deserializeUser((user, done) => {
     done(null, user);
   });
 
@@ -32,38 +34,73 @@ function configurePassport(client) {
     {
       client,
       params: {
-        scope: 'openid profile offline_access',
+        scope: 'openid profile veteran_status.read',
       },
     }, (tokenset, userinfo, done) => {
-      console.log('tokenset', tokenset);
-      console.log('userinfo', userinfo);
-      done(null, userinfo);
+      done(null, { userinfo, tokenset });
     }
   ));
-
-  return client;
 }
 
-function startApp(client) {
+const verifyVeteranStatus = async (req, res, next) => {
+  if (req.session && req.session.passport && req.session.passport.user) {
+    const veteranStatus = await new Promise((resolve, reject) => {
+      https.get(
+        'https://dev-api.va.gov/services/veteran_verification/v0/status',
+        { headers: {'Authorization': `Bearer ${req.session.passport.user.tokenset.access_token}`} },
+        (res) => {
+          let rawData = '';
+          if (res.statusCode !== 200) {
+            reject(new Error('Request Failed'));
+          }
+          res.setEncoding('utf-8');
+          res.on('data', (chunk) => { rawData += chunk; });
+          res.on('end', () => {
+            try {
+              const parsedOutput = JSON.parse(rawData);
+              resolve(parsedOutput.data.attributes.veteran_status);
+            } catch (err) {
+              reject(err);
+            }
+          });
+        }
+      ).on('error', reject);
+    });
+    res.send(veteranStatus);
+    next();
+  } else {
+    res.redirect('/auth'); // Redirect the user to login if they are not
+    next();
+  }
+};
+
+const startApp = (client) => {
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = 8080;
+  const secret = 'My Super Secret Secret'
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(session({ secret }));
 
   app.get('/', (req, res) => res.send('Hello World!'));
+  app.get('/status', verifyVeteranStatus);
+
   app.get('/auth', passport.authenticate('oidc'));
-  app.get('/auth/cb', passport.authenticate('oidc', { successRedirect: '/', failureRedirect: '/login' }));
-  app.get('/auth/refresh', (req, res, done) => {
-    const { refresh_token } = req.query;
-    client.refresh(refresh_token).then(tokenset => {
-      console.log('refreshed and validated tokens', tokenset);
-      res.send("Refreshed");
-      done();
-    });
-  });
+  app.get(
+    '/auth/cb',
+    passport.authenticate('oidc', { successRedirect: '/', failureRedirect: '/'})
+  );
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 }
 
-createClient().then(configurePassport).then(startApp);
+(async () => {
+  try {
+    const oidcClient = await createClient();
+    configurePassport(oidcClient);
+    startApp(oidcClient);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
