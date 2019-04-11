f8720a4fcb771f2dbcb84519d64608523217afb3
Philip Becker <philip.becker@oddball.io>
2019-04-11 17:34:22 -0400

    add disability_rating API and add error generation

--- samples/oauth_rails_vetverification/app/models/veteran_verification.rb
+++ samples/oauth_rails_vetverification/app/models/veteran_verification.rb
@@ -14,8 +14,32 @@ class VeteranVerification
     response['data'].collect { |data| ServiceHistory.new(data) }
   end
 
+  def disability_rating
+    response = get('disability_rating')
+    d_rating = {}
+    response['data']['attributes'].each do |key,value|
+      d_rating[key] =
+        if key == 'effective_date'
+          Time.zone.parse(value)
+        else
+          value
+        end
+    end
+    d_rating
+  end
+
 private
+  class ResultsUnavailable < StandardError
+    attr_reader :api_errors
+    def initialize(msg, errors)
+      @api_errors = errors
+      super(msg)
+    end
+  end
+
   def get(endpoint, version: 0)
-    HTTParty.get("https://#{@env_prefix}api.va.gov/services/veteran_verification/v#{version}/#{endpoint}", { headers: @authed_header })
+    response = HTTParty.get("https://#{@env_prefix}api.va.gov/services/veteran_verification/v#{version}/#{endpoint}", { headers: @authed_header })
+    return response if response.code == 200
+    raise ResultsUnavailable.new("Accessing #{endpoint} API returned an #{Rack::Utils::HTTP_STATUS_CODES[response.code]}", response['errors'])
   end
 end
