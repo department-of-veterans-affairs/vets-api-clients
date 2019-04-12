0eeaef970a7db5ce94530c901eaef1e75607c47c
Philip Becker <philip.becker@oddball.io>
2019-04-11 23:05:52 -0400

    use partials to handle error states

--- samples/oauth_rails_vetverification/app/helpers/verification_helper.rb
+++ samples/oauth_rails_vetverification/app/helpers/verification_helper.rb
@@ -1,2 +1,21 @@
 module VerificationHelper
+  def badge_class(veteran_confirmed)
+    if veteran_confirmed.is_a? VeteranVerification::BadResponse
+      'badge-danger'
+    elsif veteran_confirmed
+      'badge-success'
+    else
+      'badge-warning'
+    end
+  end
+
+  def confirmed_text(veteran_confirmed)
+    if veteran_confirmed.is_a? VeteranVerification::BadResponse
+      'Unreachable'
+    elsif veteran_confirmed
+      'Confirmed' 
+    else
+      'Unconfirmed'
+    end
+  end
 end
--- samples/oauth_rails_vetverification/app/models/veteran_verification.rb
+++ samples/oauth_rails_vetverification/app/models/veteran_verification.rb
@@ -6,16 +6,19 @@ class VeteranVerification
 
   def confirmed?
     response = get('status')
+    return response if response.is_a? BadResponse
     response['data']['attributes']['veteran_status'] == 'confirmed'
   end
 
   def service_histories
     response = get('service_history')
+    return response if response.is_a? BadResponse
     response['data'].collect { |data| ServiceHistory.new(data) }
   end
 
   def disability_rating
     response = get('disability_rating')
+    return response if response.is_a? BadResponse
     d_rating = {}
     response['data']['attributes'].each do |key,value|
       d_rating[key] =
@@ -29,17 +32,17 @@ class VeteranVerification
   end
 
 private
-  class ResultsUnavailable < StandardError
-    attr_reader :api_errors
-    def initialize(msg, errors)
+  class BadResponse
+    attr_reader :message, :api_errors
+    def initialize(message, errors)
       @api_errors = errors
-      super(msg)
+      @message = message
     end
   end
 
   def get(endpoint, version: 0)
     response = HTTParty.get("https://#{@env_prefix}api.va.gov/services/veteran_verification/v#{version}/#{endpoint}", { headers: @authed_header })
     return response if response.code == 200
-    raise ResultsUnavailable.new("Accessing #{endpoint} API returned an #{Rack::Utils::HTTP_STATUS_CODES[response.code]}", response['errors'])
+    BadResponse.new("Accessing #{endpoint} API returned an #{Rack::Utils::HTTP_STATUS_CODES[response.code]}", response['errors'])
   end
 end
