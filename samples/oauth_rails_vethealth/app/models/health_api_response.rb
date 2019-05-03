ac6cdd28a25cfde3f11ae4823a96e389eb3e5668
Philip Becker <philip.becker@oddball.io>
2019-05-03 13:49:20 -0400

    better spacing on conditional

--- samples/oauth_rails_vethealth/app/models/health_api_response.rb
+++ samples/oauth_rails_vethealth/app/models/health_api_response.rb
@@ -2,16 +2,19 @@ class HealthApiResponse
   attr_reader :api_name, :target, :page, :count
   def initialize(api_name:, id:, access_token:, action: :read, page: 1, count: 10)
     @api_name, @id, @action = api_name, id, action
-    if action == :search
+
+    case action
+    when :search
       @page, @count = page, count
       search_param = (api_name == 'Patient' ? '_id' : 'patient')
       @target = "https://dev-api.va.gov/services/argonaut/v0/#{api_name}?#{search_param}=#{id}&page=#{page}&_count=#{count}"
-    elsif action == :read
+    when :read
       @page, @count = nil, nil
       @target = "https://dev-api.va.gov/services/argonaut/v0/#{api_name}/#{id}"
     else
       raise "action must be one of the symbols :search or :read"
     end
+    
     @api_response = HTTParty.get(@target, headers: { Authorization: "Bearer #{access_token}" })
   end
 
@@ -20,9 +23,8 @@ class HealthApiResponse
   end
 
   def response_string
-    return @response_string if @response_string
     # use JSON pretty_generate to add readable whitespace and gsub api links to be links with the test app
-    @response_string = JSON.pretty_generate(@api_response.to_h).gsub(/\"(https:\/\/dev-api.va.gov\/services\/argonaut\/v0)\/(\w+)\/(.+)\"/, "<a href=\"/health_api/api_response/\\2/\\3\">\\1\/\\2\/\\3</a>")
+    @response_string ||= JSON.pretty_generate(@api_response.to_h).gsub(/\"(https:\/\/dev-api.va.gov\/services\/argonaut\/v0)\/(\w+)\/(.+)\"/, "<a href=\"/health_api/api_response/\\2/\\3\">\\1\/\\2\/\\3</a>")
   end
 
   def api_navs
