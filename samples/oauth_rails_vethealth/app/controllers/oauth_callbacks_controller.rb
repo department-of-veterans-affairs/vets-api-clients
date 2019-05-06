5be4e7035cfbd66fb0a1e8c7fa1f32e1ee5a8f16
Philip Becker <philip.becker@oddball.io>
2019-05-06 18:04:26 -0400

    separate out unverified state view

--- samples/oauth_rails_vethealth/app/controllers/oauth_callbacks_controller.rb
+++ samples/oauth_rails_vethealth/app/controllers/oauth_callbacks_controller.rb
@@ -1,6 +1,9 @@
 class OauthCallbacksController < ApplicationController
   def show
     @oauth_callback = OauthCallback.find(params[:id])
+    unless @oauth_callback.verified_state?
+      render :unverified_state and return
+    end
     @oauth_callback.authentication.validate_session(session) if @oauth_callback.authentication
   end
 end
