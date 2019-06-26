27ad929eb1313ec26d62a7b3fe5a39c6635257f9
Michael Bastos <bastosmichael@gmail.com>
2019-06-25 21:32:10 -0500

    creating seed users

--- samples/oauth_rails/VetsApiClaims/db/seeds.rb
+++ samples/oauth_rails/VetsApiClaims/db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+TestUser.create(
+  poa: '074',
+  first_name: 'Greg',
+  last_name: 'Anderson',
+  email: 'vets.gov.user+1@gmail.com'
+)
+TestUser.create(
+  poa: '083',
+  first_name: 'Andrea',
+  last_name: 'Mitchel',
+  email: 'vets.gov.user+2@gmail.com'
+)
+TestUser.create(
+  poa: '095',
+  first_name: 'Kenneth',
+  last_name: 'Andrew',
+  email: 'vets.gov.user+3@gmail.com'
+)
+TestUser.create(
+  poa: '074',
+  first_name: 'Alfred',
+  last_name: 'Armstrong',
+  email: 'vets.gov.user+4@gmail.com'
+)
+TestUser.create(
+  poa: '083',
+  first_name: 'Frank',
+  last_name: 'Lee',
+  email: 'vets.gov.user+5@gmail.com'
+)
+
+ TestUser.create(
+  poa: '2NY',
+  first_name: 'John',
+  last_name: 'Doe',
+  email: 'va.api.user+idme.007@gmail.com'
+)
+
+ TestVeteran.create(
+  poa: '2NY',
+  first_name: 'Tamara',
+  last_name: 'Ellis',
+  birth_date: '1967-06-19',
+  ssn: '796130115',
+  edipi: '1005490754'
+)
+TestVeteran.create(
+  poa: '1NY',
+  first_name: 'Janet',
+  last_name: 'Moore',
+  birth_date: '1949-05-06',
+  ssn: '796127677',
+  edipi: '1005396162'
+)
