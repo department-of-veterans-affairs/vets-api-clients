dacbacc36efe5651274067831a1a4373f8592e8c
Drew Fisher <drew.fisher@adhocteam.us>
2021-02-17 15:26:41 -0800

    Refactor to use HLR service

--- samples/oauth_rails/VetsApiAppeals/Gemfile
+++ samples/oauth_rails/VetsApiAppeals/Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'httparty'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- samples/oauth_rails/VetsApiAppeals/app/controllers/hlr_controller.rb
+++ samples/oauth_rails/VetsApiAppeals/app/controllers/hlr_controller.rb
@@ -2,52 +2,30 @@ class HlrController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def new
-    url = URI.parse'http://localhost:3000/services/appeals/v1/decision_reviews/higher_level_reviews/schema'
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    @schema = res.body
+    @schema = service.schema
+    @header_schema = service.header_schema
   end
 
   def create
-    uri = URI.parse 'http://localhost:3000/services/appeals/v1/decision_reviews/higher_level_reviews/'
-    http = Net::HTTP.new uri.host, uri.port
-    request = Net::HTTP::Post.new uri.request_uri
-    p = params.permit!.to_h.with_indifferent_access
-
-    form_headers = p.delete('hlrCreateParameters')
-    form_headers.each do |header, value|
-      request[header] = value
-    end
-
-    form_data = { data: p.delete(:data), included: p.delete(:included) }
-    form_data[:data][:attributes][:informalConference] = (form_data[:data][:attributes][:informalConference] == 'true')
-    form_data[:data][:attributes][:sameOffice] = (form_data[:data][:attributes][:sameOffice] == 'true')
-
-    # Convert included from an array-like hash (e.g. { "0" => {...}, "1": {...}, ...} ) to an array of objects
-    included_params = form_data.delete(:included)
-    form_data[:included] = []
-    included_params.each do |i, data|
-      form_data[:included] << data
-    end
-
-    request.body = form_data.to_json
-    request['Content-Type'] = 'application/json'
-    res = http.request(request)
-    render json: res.body
+    render json: service.post(hlr_params).body
   end
 
   def show
-    url = URI.parse"http://localhost:3000/services/appeals/v1/decision_reviews/higher_level_reviews/#{params[:id]}"
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    tmp = JSON.parse(res.body)['data']
-    @hlr = {}
-    @hlr['id'] = tmp['id']
-    @hlr['type'] = tmp['type']
-    @hlr.merge! tmp['attributes'].except('formData')
+    data = service.get(params[:id]).except('formData')
+    @hlr = { ID: data['id'],
+             Type: data['type'],
+             Status: data.dig('attributes', 'status'),
+             'Created At': data.dig('attributes', 'createdAt'),
+             'Updated At': data.dig('attributes', 'updatedAt') }
+  end
+
+  private
+
+  def hlr_params
+    params.permit!.to_h
+  end
+
+  def service
+    @service ||= HlrService.new
   end
 end
