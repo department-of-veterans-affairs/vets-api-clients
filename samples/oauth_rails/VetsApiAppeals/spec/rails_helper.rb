d6a82fa4bddd15746570e5ac089cfc1900c6a7d8
nathanbwright <nathanielbwright@gmail.com>
2021-03-11 09:18:16 -0700

    Basic specs for HlrService

--- samples/oauth_rails/VetsApiAppeals/spec/rails_helper.rb
+++ samples/oauth_rails/VetsApiAppeals/spec/rails_helper.rb
@@ -7,6 +7,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/support/vcr_cassettes"
+  config.hook_into :webmock
+  config.ignore_localhost = true
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
