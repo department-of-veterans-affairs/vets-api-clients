3af6568f99d758be26e3a09e44384c7c06f6163a
Beau Grantham <beau.grantham@va.gov>
2021-04-15 16:29:49 -0400

    Set default session values

--- samples/oauth_node/index.js
+++ samples/oauth_node/index.js
@@ -114,7 +114,7 @@ const startApp = (client) => {
   app.set('view engine', 'ejs')
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(session({ secret, cookie: { maxAge: 60000 } }));
+  app.use(session({ secret, cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }));
   app.use(bodyParser.json()); // support json encoded bodies
   app.use(bodyParser.urlencoded({ extended: true }));
 
