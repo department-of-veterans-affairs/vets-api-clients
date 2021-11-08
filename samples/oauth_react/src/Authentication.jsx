e9629e78ca5afab0016566bc3e041a00609b3671
dbrown-va <43859081+dbrown-va@users.noreply.github.com>
2021-11-08 10:02:50 -0500

    Revert "API-8810: Archive all Single Page Apps (SPA) from Sample App Collection"

---
+++ samples/oauth_react/src/Authentication.jsx
@@ -0,0 +1,143 @@
+// This module exports the Authentication element to manage the various
+// openid-client objects required to log in and out. It also exports a variety
+// of elements to make it easier to consume the authentication context without
+// having to care about AuthenticationContext.
+
+import React, { Component } from 'react';
+import { Log, UserManager } from 'oidc-client';
+
+const AuthenticationContext = React.createContext({user: null});
+
+class Authentication extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {authenticated: false, user: null};
+    this.userManager = new UserManager(this.props.oidcSettings);
+    this.userManager.events.addUserLoaded((user) => {
+      console.log('user loaded, changing Authentication state');
+      this.setState({authenticated: true, user: user});
+    });
+    this.userManager.events.addUserUnloaded(() => {
+      console.log('user unloaded, changing Authentication state');
+      this.setState({authenticated: false, user: null});
+    });
+  }
+
+  componentDidMount() {
+    Log.logger = window.console;
+    this.userManager.getUser().then((user) => {
+      this.setState({authenticated: true, user: user});
+      console.log(user)
+    });
+  }
+
+  render() {
+    const ctxValue = {user: this.state.user, userManager: this.userManager, oidcSettings: this.props.oidcSettings};
+    return (
+      <AuthenticationContext.Provider value={ctxValue}>
+        {this.props.children}
+      </AuthenticationContext.Provider>
+    );
+  }
+}
+
+class ImplicitCallback extends Component {
+  static contextType = AuthenticationContext;
+
+  componentDidMount() {
+    this.context.userManager.signinRedirectCallback()
+      .then((user) => this.onRedirectSuccess(user))
+      .catch((error) => this.onRedirectError(error));
+  }
+
+  onRedirectSuccess(user) {
+    console.log(user);
+    this.props.history.push(`/`)
+  }
+
+  onRedirectError(error) {
+    console.log(error);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+// Renders it's children only if the authentication context can provides an
+// authenticated user. That user object is passed to the children of the
+// SignedIn element as a prop. This allows any element that requires access to
+// the authenticated user to obtain it without having to integrate with the
+// AuthenticationContext.
+class SignedIn extends Component {
+  static contextType = AuthenticationContext;
+
+  // Renders it's children with a `user` prop. This allows any component that
+  // requires access to the authenticated
+  render() {
+    if (this.context.user == null) {
+      return null;
+    } else {
+      return React.Children.map(this.props.children, (child, index) => {
+        return React.cloneElement(child, { index, user: this.context.user });
+      });
+    }
+  }
+
+  // Returns a function that serves as a stateless component that renders it's
+  // children inside a <SignedIn> element.
+  static wrapComponent(ComponentClass) {
+    return (props) => { return (<SignedIn><ComponentClass {...props} /></SignedIn>) };
+  }
+}
+
+// Render's it's children only if the authentication context cannot provide an
+// authenticated user.
+class SignedOut extends Component {
+  static contextType = AuthenticationContext;
+
+  render() {
+    if (this.context.user == null) {
+      return this.props.children;
+    } else {
+      return null;
+    }
+  }
+}
+
+// Intended to be used as a base class for an interactive UI element. Renders
+// an element of the type named by it's `as` prop, with an overridable
+// `onClick` event handler.
+class ClickableMimic extends Component {
+  constructor(props) {
+    if (props.as == null) {
+      throw new Error("the `as` property is required.");
+    }
+
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  render() {
+    const ElementType = this.props.as;
+    return (
+      <ElementType onClick={this.onClick} {...this.props}>{this.props.children}</ElementType>
+    );
+  }
+}
+
+class SignInButton extends ClickableMimic {
+  static contextType = AuthenticationContext;
+  onClick() {
+    this.context.userManager.signinRedirect();
+  }
+}
+
+class SignOutButton extends ClickableMimic {
+  static contextType = AuthenticationContext;
+  onClick() {
+    this.context.userManager.signoutRedirect();
+  }
+}
+
+export { Authentication, ImplicitCallback, SignInButton, SignOutButton, SignedIn, SignedOut };
---
+++ samples/oauth_react/src/Navbar.jsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
+ * This file has been modified from the original.
+ * Modifications are subject to the CC0 1.0 Universal public domain dedication.
+ *
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import React, { Component } from 'react';
+import { Container, Image, Menu } from 'semantic-ui-react';
+import { SignInButton, SignOutButton, SignedIn, SignedOut } from './Authentication';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <Menu fixed="top" inverted>
+          <Container>
+            <Menu.Item as="a" header href="/">
+              <Image size="mini" src="/adhoc.png" />
+              &nbsp;&nbsp;
+              Sample VA API App
+            </Menu.Item>
+            <SignedIn>
+              <Menu.Item id="servicehistory-button" as="a" href="/servicehistory">Service History</Menu.Item>
+              <Menu.Item id="disability-button" as="a" href="/disabilityrating">Disability Rating</Menu.Item>
+              <Menu.Item id="profile-button" as="a" href="/profile">Profile</Menu.Item>
+              <Menu.Item id="logout-button" as="a"><SignOutButton as="a">Log Out</SignOutButton></Menu.Item>
+            </SignedIn>
+            <SignedOut>
+              <Menu.Item as="a"><SignInButton as="a">Log In</SignInButton></Menu.Item>
+            </SignedOut>
+          </Container>
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
