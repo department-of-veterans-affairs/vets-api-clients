6682cc94c54cb17cafa59452484c4ecc5ec68aa1
Derek Brown <derek.brown@libertyits.com>
2022-02-18 11:18:37 -0500

    API-000 Removed legacy OAuth applications

--- samples/oauth_rails/VetsApiAppeals/app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  before_action :set_apikey
-
-  private
-
-  def set_apikey
-    @apikey = session[:apikey]
-  end
-end
--- samples/oauth_rails/vetverification/config/initializers/figaro.rb
+++
@@ -1,4 +0,0 @@
-Figaro.require_keys(
-  'va_developer_client_id',
-  'va_developer_client_secret'
-)
